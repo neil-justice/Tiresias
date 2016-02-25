d8f6ad774658423ad557c108581abe80bd077c71
Neil Justice <neil-justice@users.noreply.github.com>
2016-02-25 19:50:55 +0000

    split script up into functions, used QuerySelectorAll instead of getting by class, added event listener instead of onLoad in the body html tag

--- scripts/nav-height.js
+++ scripts/nav-height.js
@@ -1,18 +1,40 @@
-function setNavElementHeights() {
+"use strict"
+addEventListener('load', start);
+
+function start()
+{
+  setNavElementHeights();
+}
+
+function setNavElementHeights() 
+{
   var navHeight = document.getElementById("navBar").clientHeight;
+  
   if (!navHeight) {
     return undefined;
   }
+  
+  setNavButtonVertAlign(navHeight);
+  setNavLinkPadding(navHeight);
+}
+
+function setNavButtonVertAlign(navHeight) 
+{
   var btnHeight = document.getElementById("nav-button").clientHeight;
   var height = (navHeight - btnHeight) / 2;
   var newHeight = height.toString() + "px";
   
   var list = document.getElementsByClassName("nav-button-padding");
+  
   for( var i = 0; i < list.length; i++) {
       list[i].style.height = newHeight;
   }
+}
+
+function setNavLinkPadding(navHeight)
+{
+  var links = document.querySelectorAll("nav li > a");
   
-  var links = document.getElementsByClassName("nav-link");
   for( var i = 0; i < links.length; i++) {
       var h = (navHeight - links[i].offsetHeight) / 2;
       var newLinkHeight = h.toString() + "px";
