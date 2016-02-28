2c6fb0547a8653dd871f1fa8d6f497065e057849
Berk M <berkmolla@gmail.com>
2016-02-28 19:53:08 +0000

    Add marker to map. Change z-index for navBar to 1 and z-index for map back to the default 0 (otherwise it wasn't possible to interact with the map.

--- scripts/map.js
+++ scripts/map.js
@@ -1,9 +1,15 @@
 window.addEventListener('load', initMap);
 
 var map;
+var bristol = {lat: 51.4500, lng: -2.5833};
 function initMap() {
     map = new google.maps.Map(document.querySelector('#map'), {
-        center: {lat: 51.4500, lng: -2.5833},
+        center: bristol,
         zoom: 10
     });
+
+    var marker = new google.maps.Marker({
+        position: bristol
+    });
+    marker.setMap(map);
 }
