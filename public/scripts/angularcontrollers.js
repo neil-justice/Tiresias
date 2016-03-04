9dc1108c303f6f3f3683ed268c71680f57c6dbfd
Berk M <berkmolla@gmail.com>
2016-03-04 19:13:04 +0000

    It works! Able to send json data to angular. Made html file into ejs template instead (don't have to change anything except file extension) with angular tags inside

--- public/scripts/angularcontrollers.js
+++ public/scripts/angularcontrollers.js
@@ -2,7 +2,7 @@ var homepageApp = angular.module('homepageApp', []);
 
 homepageApp.controller('homepageController', function($scope, $http) {
 
-    $http.get('/homepage')
+    $http.get('/homepagedata')
         .success(function(data) {
             $scope.predictions = data;
             console.log(data);
@@ -21,23 +21,10 @@ homepageApp.controller('homepageController', function($scope, $http) {
             'name':'Contact'
         },
         {
-            'name':'Bla'
+            'name':'OTHERTITLE!'
         },
         {
             'name':'Hello'
         }];
 
-    $scope.predictions = [
-        {
-            'title':'Title1',
-            'link':'http://www.bit.ly/49833'
-        },
-        {
-            'title':'Title2',
-            'link':'bit.ly/43433'
-        },
-        {
-            'title':'Title3',
-            'link':'bit.ly/49593'
-        }]
 });
