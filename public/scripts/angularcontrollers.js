d6685d117ea2168df5766e62640d9e5e687cfd4d
Berk M <berkmolla@gmail.com>
2016-03-08 10:24:22 +0000

    Delete locationProvider code. Remove angular-route from bower.json file.

--- public/scripts/angularcontrollers.js
+++ public/scripts/angularcontrollers.js
@@ -1,12 +1,4 @@
-var homepageApp = angular.module('homepageApp', ['ngRoute', 'ngResource']);
-
-// homepageApp.config(function ($locationProvider) {
-
-//     $locationProvider.html5Mode({
-//             enabled: true,
-//             requireBase: false
-//     });
-// });
+var homepageApp = angular.module('homepageApp', ['ngResource']);
 
 homepageApp.factory('Prediction', function($resource) {
      return $resource("/api/predictions/:pid");
@@ -26,25 +18,9 @@ homepageApp.controller('homepageController', function($scope, $http) {
 homepageApp.controller('predictionsController', ['$scope', '$window', 'Prediction', function($scope, $window, Prediction) {
  
     var pId = $window.location.pathname.split("/")[2]||"Unknown";
-    console.log(pId);
+
     var entry = Prediction.get({ pid: pId }, function() {
         $scope.title = entry['title'];
         $scope.link = entry['link'];
     });
-
-
-
-    // $http.get('/api/predictions/:pid', { 
-    //         params: {
-    //             id: '56d9ec10af5da8a69d144ed4'
-    //     }})
-    //     .success(function(data) {
-    //         var title = data['title'];
-    
-    //         $scope.title = title;
-    //         console.log($scope.title);
-    //         console.log("successful id fetch");
-    // });
-
-
 }]);
