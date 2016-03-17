3899af9fcf449234632988e85836920cef12c4c8
neil-justice <neil-justice@users.noreply.github.com>
2016-03-17 17:16:43 +0000

    added svg progress bar animation and colour change

--- public/scripts/progressBar.js
+++ public/scripts/progressBar.js
@@ -4,7 +4,7 @@ eventListener('load', calcProgress, window);
 function calcProgress()
 {
   // This is a dummy value while JSON objects don't contain a date
-  setProgress(90);
+  setProgress(100);
 }
 
 function setProgress(percent)
@@ -12,7 +12,22 @@ function setProgress(percent)
   if(percent > 100) percent = 100;
   if(percent < 0) percent = 0;
 
-  var bar = document.querySelector("#svg-bar");
+  var color = [
+      '#00FF00',
+      '#76ff03',
+      '#64dd17',
+      '#c6ff00',
+      '#eeff41',
+      '#ffeb3b',
+      '#fdd835',
+      '#f9a825',
+      '#d84315',
+      '#ff3d00',
+      '#FF0000',]
+
+  var animate = document.querySelector("#svg-animate");
+  var animateColor = document.querySelector("#svg-animate-color");
+
   var cont = document.querySelector("#svg-border");
   var rect = cont.getBoundingClientRect();
   var contWidth = rect.right - rect.left;
@@ -20,5 +35,6 @@ function setProgress(percent)
   var maxBarWidth = contWidth * 0.9737669854;
   var width = maxBarWidth / (100 / percent);
 
-  bar.setAttribute("width",  width + 'px');
+  animate.setAttribute("to",  width + 'px');
+  animateColor.setAttribute("to",  color[Math.floor(percent / 10)]);
 }
