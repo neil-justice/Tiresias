8b4d53d259ad1aadd241d7581047a04a55545056
Berk M <berkmolla@gmail.com>
2016-04-05 23:07:08 +0200

    Fix submit prediction form not being able to find google maps API.

--- public/scripts/angularcontrollers.js
+++ public/scripts/angularcontrollers.js
@@ -51,7 +51,7 @@ homepageApp.factory('loadGoogleMapAPI', ['$window', '$q', function ($window, $q)
 
 
 // Navbar stuff
-homepageApp.controller('navController', function($scope, Prediction, predictions) {
+homepageApp.controller('navController', function($scope, Prediction, predictions, loadGoogleMapAPI) {
 
     $scope.newPrediction = new Prediction();
 
@@ -81,44 +81,50 @@ homepageApp.controller('navController', function($scope, Prediction, predictions
 
     $scope.submitPrediction = function(form) {
 
-        // For coordinate lookup
-        var geocoder = new google.maps.Geocoder();
+        loadGoogleMapAPI.then(function success() {
 
-        // Look up coordinate of given value in 'Location'
-        geocoder.geocode({'address': $scope.newPrediction.location}, function(results, status) {
-            
-            // If response is OK, alter the location property to contain LatLng instead.
-            if (status === google.maps.GeocoderStatus.OK) {
-                $scope.newPrediction.location = results[0].geometry.location;
-            } else {
-                // Remove invalid location
-                delete $scope.newPrediction.location;
-            }
+            // For coordinate lookup
+            var geocoder = new google.maps.Geocoder();
 
-            var dataBeforeSave = angular.copy($scope.newPrediction.toJSON());
-
-            // Save new prediction data
-            $scope.newPrediction.$save(function successCallback(res) {
-
-                // $save updates the newPrediction resource automatically with the response, and so the ng-model
-                // in the html is also updated due to two-way binding. It then complains that the date is an incorrect
-                // format cause mongoDB stores the date as ISODate whereas the input type='date' html expects a normal
-                // yyyy-mm-dd format.
-                // That's why the contents of newPrediction are saved into a new variable above and added to the predictions.list
-                // service here rather than newPrediction itself.
-                dataBeforeSave._id = res._id;
-                predictions.list.push(dataBeforeSave);
-
-                // Clear contents of newPrediction so that the form is not populated next time.
-                $scope.newPrediction = new Prediction();
-                $scope.closePredictionWindow(form);
-                $scope.addNotification("Prediction added successfully!", 'success-notification');
+            // Look up coordinate of given value in 'Location'
+            geocoder.geocode({'address': $scope.newPrediction.location}, function(results, status) {
                 
-            }, function errorCallback(res) {
-                console.log('Error: ' + res);
-                $scope.addNotification("Error: Prediction could not be added!", 'failure-notification');
-            });            
-        });
+                // If response is OK, alter the location property to contain LatLng instead.
+                if (status === google.maps.GeocoderStatus.OK) {
+                    $scope.newPrediction.location = results[0].geometry.location;
+                } else {
+                    // Remove invalid location
+                    delete $scope.newPrediction.location;
+                }
+
+                var dataBeforeSave = angular.copy($scope.newPrediction.toJSON());
+
+                // Save new prediction data
+                $scope.newPrediction.$save(function successCallback(res) {
+
+                    // $save updates the newPrediction resource automatically with the response, and so the ng-model
+                    // in the html is also updated due to two-way binding. It then complains that the date is an incorrect
+                    // format cause mongoDB stores the date as ISODate whereas the input type='date' html expects a normal
+                    // yyyy-mm-dd format.
+                    // That's why the contents of newPrediction are saved into a new variable above and added to the predictions.list
+                    // service here rather than newPrediction itself.
+                    dataBeforeSave._id = res._id;
+                    predictions.list.push(dataBeforeSave);
+
+                    // Clear contents of newPrediction so that the form is not populated next time.
+                    $scope.newPrediction = new Prediction();
+                    $scope.closePredictionWindow(form);
+                    $scope.addNotification("Prediction added successfully!", 'success-notification');
+                    
+                }, function errorCallback(res) {
+                    console.log('Error: ' + res);
+                    $scope.addNotification("Error: Prediction could not be added!", 'failure-notification');
+                });            
+            });
+        }, function error() {
+            console.log("Failed to load google maps API");
+        })
+
     };
 });
 
