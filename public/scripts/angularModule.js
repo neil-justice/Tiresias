d4bb61c6f2376305af26995c67e84c1ad8746fde
neil-justice <neil-justice@users.noreply.github.com>
2016-05-20 16:08:21 +0100

    refactored angular controllers, gave them a file each

---
+++ public/scripts/angularModule.js
@@ -0,0 +1,117 @@
+var homepageApp = angular.module('homepageApp', ['ngResource', 'ngRoute', 'ngMessages', 'growlNotifications', 'ngAnimate']);
+
+homepageApp.factory('Prediction', function($resource) {
+     return $resource("/api/predictions/:pid");
+ });
+
+homepageApp.factory('User', function($resource) {
+     return $resource("/api/signup");
+ });
+
+homepageApp.factory('authentication', function($http, $window) {
+    var saveToken = function (token) {
+        $window.localStorage['token'] = token;
+    };
+
+    var getToken = function() {
+        return $window.localStorage['token'];
+    };
+
+    var logout = function() {
+        $window.localStorage.removeItem('token');
+    };
+
+    var verifyUser = function() {
+            return $http({
+                method: 'POST',
+                url: '/api/decode',
+                data: {
+                    token: getToken()
+                }
+            }).then(function successCallback(res) {
+                console.log('logged in as: ' + res.data.username +
+                            ' email: ' + res.data.email +
+                            ' successes: ' + res.data.successCount +
+                            ' fails: ' + res.data.failCount);
+                return { isLoggedIn: true,
+                         currentUser: { username: res.data.username,
+                                        email: res.data.email,
+                                        successCount: 0,
+                                        failCount: 0 }
+                        };
+            }, function errorCallback(res) {
+                console.log('not logged in: ' + res.data.message);
+                return {
+                        isLoggedIn: false,
+                        currentUser: {}
+                        }
+            });
+    }
+
+    return {
+        saveToken: saveToken,
+        getToken: getToken,
+        logout: logout,
+        verifyUser: verifyUser
+    };
+});
+
+homepageApp.factory('notifications', function() {
+    var notificationIndex = 0; //ID for notifications
+    var notifications = {}; // list of notifications
+
+    var addNotification = function (notification, style) {
+      var i = notificationIndex++;
+      notifications[i] = { text: notification, style: style };
+    };
+
+    return {
+        addNotification: addNotification,
+        notifications: notifications
+    }
+});
+
+homepageApp.config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
+
+    $routeProvider.
+        when('/predictions/:pid', {
+            templateUrl: '/views/prediction.html',
+            controller: 'predictionsController'
+        }).
+        when('/', {
+            templateUrl: '/views/homepage.html',
+            controller: 'homepageController'
+        }).
+        otherwise({
+            redirectTo: '/'
+        });
+}]);
+
+homepageApp.factory('predictions', function() {
+    return {
+        list: []
+    };
+});
+
+// Lazy loading of Google Map API - only gets called the first time and appends the google maps script to the head of the page.
+homepageApp.factory('loadGoogleMapAPI', ['$window', '$q', function ($window, $q) {
+
+        var deferred = $q.defer();
+
+        // Load Google map API script
+        function loadScript() {
+            var script = document.createElement('script');
+            script.src = "https://maps.googleapis.com/maps/api/js?key=AIzaSyAg67S7m3vG4o51-RyozMWZ1mtmzSIS-1o&callback=scriptLoaded";
+
+            document.head.appendChild(script);
+        }
+
+        // Script loaded callback, send resolve
+        $window.scriptLoaded = function () {
+            deferred.resolve();
+        }
+
+        loadScript();
+
+        return deferred.promise;
+}]);
---
+++ public/scripts/controllers/predictionController.js
@@ -0,0 +1,58 @@
+// Single prediction page
+homepageApp.controller('predictionsController', ['$scope', '$window', '$routeParams', '$location', 'Prediction', 'loadGoogleMapAPI', '$http',
+    function($scope, $window, $routeParams, $location, Prediction, loadGoogleMapAPI, $http) {
+
+    // Gets the prediction's _id value from the url
+    var pId = $routeParams.pid;
+
+    // Request the prediction with that _id from the database
+    $scope.entry = Prediction.get({ pid: pId }, function(data, headers) {
+        $scope.title = $scope.entry['title'];
+        $scope.link = $scope.entry['link'];
+        $scope.description = $scope.entry['description'];
+        $scope.tags = $scope.entry['tags'];
+        $scope.votes = $scope.entry['votes'];
+
+        if (!$scope.votes) {
+            $scope.votes = 0;
+        }
+
+        // Load google maps API and if successful, create map with location details of the prediction.
+        loadGoogleMapAPI.then(function success() {
+
+            // Get location and create map/marker
+            var location = $scope.entry['location'];
+            var lat = location['lat'];
+            var lng = location['lng'];
+            var latlng = new google.maps.LatLng(lat, lng);
+            $scope.map = initMap(latlng);
+            createMarker($scope.map, location);
+        }), function error() {
+            console.log("Error loading google maps API script");
+        };
+
+        $scope.comments = $scope.entry['comments'];
+        $scope.startDate = $scope.entry['startDate'];
+        $scope.endDate = $scope.entry['endDate'];
+        var start = $scope.startDate;
+        var end = $scope.endDate;
+        calcProgress(start, end);
+    }, function error(res) {
+        $location.path('/').replace();
+    });
+
+    $scope.sendVote = function(vote) {
+        $http({
+            method: 'POST',
+            url: '/api/vote',
+            data: {
+                vote: vote,
+                _id: pId
+            }
+        }).then(function successCallback(res) {
+            console.log('Vote successfully counted ' + res.status);
+        }, function errorCallback(res) {
+            console.log('Failed to vote' + res.status);
+        });
+    }
+}]);
