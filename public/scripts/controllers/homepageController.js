8378e08309e822542a85e51b7a8a229ccf2f5bfe
Berk M <berkmolla@gmail.com>
2016-05-23 15:05:47 +0100

    Add tag filtering and filter clearing. Need to place clear filters button properly.

--- public/scripts/controllers/homepageController.js
+++ public/scripts/controllers/homepageController.js
@@ -1,29 +1,7 @@
 // controller for the front page view
 homepageApp.controller('homepageController', function($scope, $window, Prediction, predictions) {
 
-    
-    // $http({method: 'GET',
-    //     url:'/predictions',
-    //     headers: {
-    //         accept:'application/json'
-    //     }
-    //     })
-    //     .then(function successCallback(res) {
-
-    //         if (res.status === 200) {
-
-    //             $scope.predictions = res.data;
-
-    //             // Turn ISODate back into normal date object ready to display
-    //             angular.forEach($scope.predictions, function(prediction) {
-    //                 if (prediction.date !== undefined) {
-    //                     prediction.date = new Date(prediction.date).toLocaleString();
-    //                 }
-    //             });
-    //         }
-    //     }, function errorCallback(res) {
-    //         console.log('Error: ' + res);
-    //     });
+    $scope.tagFilters = {};
 
     Prediction.query({}, function(data) {
 
@@ -33,11 +11,6 @@ homepageApp.controller('homepageController', function($scope, $window, Predictio
             var prediction = predictionResource.toJSON();
             predictions.list.push(prediction);
 
-            // Date formatting
-            // if (prediction.date !== undefined) {
-            //     prediction.date = new Date(prediction.date).toLocaleString();
-            // }
-            console.log(prediction.dateAdded);
             prediction.dateAdded = moment(prediction.dateAdded).format("Do MMM YYYY");
 
             // Number of comments
@@ -62,5 +35,13 @@ homepageApp.controller('homepageController', function($scope, $window, Predictio
         banner.style.top = offset + "px";
         
     });
+
+    $scope.tagsAreEmpty = function() {
+        return angular.equals({}, $scope.tagFilters);
+    }
+
+    $scope.clearTagFilters = function() {
+        $scope.tagFilters = {};
+    }
 
 });
