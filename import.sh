0ba6a764dc60f06bd3d3e3f45314229ef12ce3d9
Berk M <berkmolla@gmail.com>
2016-05-25 23:01:36 +0100

    Update import script to import stats collection. Dump database so that stats.bson is created.

--- import.sh
+++ import.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 eval $(echo "mongorestore --drop --db tiresias mongobackup/tiresias/predictions.bson")
 eval $(echo "mongorestore --drop --db tiresias mongobackup/tiresias/users.bson")
+eval $(echo "mongorestore --drop --db tiresias mongobackup/tiresias/stats.bson")
